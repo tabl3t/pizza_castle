e07788546f06a8cf644c09d004b09820fe2ee10e
Shahriar Islam Tamim <tamimislam254@gmail.com>
2026-05-03 02:42:36 +0600

    db.php

--- db.php
+++ db.php
@@ -1,19 +1,10 @@
 <?php
-
-$host = "mysql.railway.internal";
-
-$user = "root";                     
-
-$pass = "wgoSYVkZwaKMnSzDtNzpKyswivbHORxf";          
-
-$db   = "railway"; 
+$host = getenv('MYSQL_HOST');
+$user = getenv('MYSQL_USER');
+$pass = getenv('MYSQL_PASSWORD');
+$db   = getenv('MYSQL_DATABASE');
 
 $conn = mysqli_connect($host, $user, $pass, $db);
-
-if (!$conn) {
-
-    die("Connection failed: " . mysqli_connect_error());
-
-}
+?>
 
 ?>
